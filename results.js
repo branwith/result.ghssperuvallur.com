02e8023fd1180592a286c62bfbb7c6eb5f63f6f2
HASSAN A <87488089+branwith@users.noreply.github.com>
2023-11-11 16:41:58 +0530

    Add files via upload

---
+++ results.js
@@ -0,0 +1,28 @@
+//Grand Total
+let equalityGrand = 13;
+let eqScore = document.getElementById("eq_Score");
+eqScore.innerHTML = equalityGrand;
+
+let libertyGrand = 21;
+let lbScore = document.getElementById("lb_Score");
+lbScore.innerHTML = libertyGrand;
+
+let fraternityGrand = 18;
+let frScore = document.getElementById("fr_Score");
+frScore.innerHTML = fraternityGrand;
+
+                            
+                                            // Results Page Data //
+
+//Bidaya
+let equalityBidaya = 5;                                             //Equality Bidaya
+let eqBdScore = document.getElementById("eqBd_Score");
+eqBdScore.innerHTML = equalityBidaya;
+
+let libertyBidaya = 0;
+let lbBdScore = document.getElementById("lbBd_Score");
+lbBdScore.innerHTML = libertyBidaya;
+
+let fraternityBidaya = 18;
+let frBdScore = document.getElementById("frBd_Score");
+frBdScore.innerHTML = fraternityBidaya;
